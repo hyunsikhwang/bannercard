36d3bcb220b7ca5a07f65e7fcad1aab737d7c4f4
Hyunsik Hwang <gomting@naver.com>
2024-01-13 13:57:53 +0900

    set_page_config

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,10 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+st.set_page_config(page_title="Gradient Banner Card",
+                   page_icon=":random:",
+                   layout='wide')
+
 local_css("style.css")
 
 st.markdown('''
